7a9956af04828c56c514c19d7547f71ca9ba813d
Ivan Palencia <ivanpalenciab@gmail.com>
2022-05-20 21:23:33 -0500

    Terminamos la primera funcion y estamos intentando hacer la segunda

--- Ciclo_1/autopartes.py
+++ Ciclo_1/autopartes.py
@@ -1,35 +1,52 @@
 def AutoPartes(ventas:list):
     diccionario = {}
-    for i in ventas:
-        id_producto = i[0]
-        description_producto=i[1]
-        numero_parte_producto= i[2]
-        cantidad_vendida_producto = i[3]
-        stock_producto = i[4]
-        nombre_comprador = i[5]
-        cedula_comprador = i[6]
-        fecha_venta = i[7]
+    IDs_producto=[i[0] for i in ventas if True]
+    descripciones_prod=[i[1] for i in ventas if True]
+    partes_prod=[i[2] for i in ventas if True]
+    cant_vend=[i[3] for i in ventas if True]
+    stock_prod=[i[4] for i in ventas if True]
+    nombre_compradores=[i[5] for i in ventas if True]
+    IDs_compradores=[i[6] for i in ventas if True]
+    fechas_venta=[i[7] for i in ventas if True]
 
 
-        IDs =[] ; descripciones=[]
+        
 
-        diccionario['Id']=id_producto ; diccionario['descripcion_producto']=description_producto;diccionario['numero_parte_producto']=numero_parte_producto
-        diccionario['cant_prod_vend']= cantidad_vendida_producto; diccionario['stock_´roducto']=stock_producto
-        diccionario['nombre_comprador']=nombre_comprador ; diccionario['Cedeula_comprador']=cedula_comprador
-        diccionario['Fecha_venta']=fecha_venta
+    diccionario['Id']=IDs_producto ; diccionario['descripcion_producto']=descripciones_prod;diccionario['numero_parte_producto']=partes_prod
+    diccionario['cant_prod_vend']= cant_vend; diccionario['stock_´roducto']=stock_prod
+    diccionario['nombre_comprador']=nombre_compradores ; diccionario['Cedeula_comprador']=IDs_compradores
+    diccionario['Fecha_venta']=fechas_venta
     
-    #print(diccionario)
+    return diccionario
 
 
 def consultaRegistro(ventas,idProducto):
-    registro = ventas['producto consultado']
-    for i in registro:
-        id_producto = i[0]
+    cont = 0
+    for i in ventas['Id']:
+        if i == idProducto:
+            id = i
+            dproduct=ventas['descripcion_producto'][cont]
+            npartes = ventas['numero_parte_producto'][cont]
+            cant_ven = ventas['cant_prod_vend'][cont]
+            stock=ventas['stock_´roducto'][cont]
+            nombre_comprador = ventas['nombre_comprador'][cont]
+            cedula_comprador = ventas['Cedeula_comprador'][cont]
+            fecha=ventas['Fecha_venta'][cont]
+            cont +1
+            
+
+            print( f'Producto consultado : {id} Descripción {dproduct} #Parte {npartes} Cantidad vendida {cant_ven} Stock {stock} Comprador {nombre_comprador} Documento {cedula_comprador} Fecha Venta {fecha}')
+            break
+
+        else:
+            print('No hay registro de venta de ese producto')
+
          
 
 
 lista = [(10,'cohete militar',1,2,200,'Vladimir putin',100,'12/06/2020'),(100,'ametralladora ligera',2,5,1000,'Timochenkp',22448475,'15/02/2001'),(1,'Bomba nuclear',5,1,4,'Donald Trump',25487,'15/09/2018')]
-AutoPartes(lista)
+registro = AutoPartes(lista)
 
-lista_2 = [i[0] for i in lista if True]
-print(lista_2)
+consulta =consultaRegistro(registro,1)
+#consultaRegistro(registro,2)
+print(len(registro['Id']))
